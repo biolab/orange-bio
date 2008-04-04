287b56e5f9f51b23fab41813c74a83c83dc1265d
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2008-04-04 11:32:00 +0000

    added orngData

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(name = "Genomics",
       packages = [ 'widgets', 'doc' ],
       package_data = {'widgets': ['icons/*.png'], 'doc': docFiles},
       extra_path="Genomics",
-      py_modules = [ 'orngKEGG', 'orngGsea', 'orngGeneMatcher' ],
+      py_modules = [ 'orngKEGG', 'orngGsea', 'orngGeneMatcher' 'orngData'],
       scripts=["registerWidgets.py"]
       )
