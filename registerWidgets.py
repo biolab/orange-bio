515a6c1acb98846bfa53862d2c7ccf9529c57acd
markotoplak <devnull@localhost>
2008-05-21 10:42:26 +0000

    Corrected path generation in register widgets.

--- registerWidgets.py
+++ registerWidgets.py
@@ -2,5 +2,8 @@ import distutils
 import distutils.sysconfig
 import sys
 import orngRegistry
+import os
 
-orngRegistry.addWidgetCategory("Orange Genomics", distutils.sysconfig.get_python_lib() + r"\Genomics\widgets", "remove" not in sys.argv[1] if len(sys.argv)>1 else 1)
+orngRegistry.addWidgetCategory("Orange Genomics", \
+    os.path.join(distutils.sysconfig.get_python_lib(),"Genomics","widgets"), \
+    "remove" not in sys.argv[1] if len(sys.argv)>1 else 1)
