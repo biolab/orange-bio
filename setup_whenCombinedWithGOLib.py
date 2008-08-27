72a97ad3c5f4e0c6fbe510ec4ff3662389285417
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2008-08-27 10:28:53 +0000

    -added obiGO module (contains genomics update functions for GOLib)

--- setup_whenCombinedWithGOLib.py
+++ setup_whenCombinedWithGOLib.py
@@ -28,6 +28,6 @@ setup(name = "Genomics",
       packages = [ 'widgets', 'doc', 'data' ],
       package_data = {'widgets': ['icons/*.png'], 'doc': docFiles},
       extra_path="Genomics",
-      py_modules = [ 'go', 'obiKEGG', 'obiGsea', 'obiGeneMatch', 'obiData', 'obiGenomisUpdate', 'stats', 'pstat', 'obiExpression' ],
+      py_modules = [ 'go', 'obiKEGG', 'obiGsea', 'obiGeneMatch', 'obiData', 'obiGenomisUpdate', 'stats', 'pstat', 'obiExpression', 'obiGO' ],
       scripts=["registerWidgets.py", "post_install_script.py"]
       )
