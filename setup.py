098b51cc77aa35e4c46b8d0b1a9dffdd3a9ef8ca
tomazc <tomaz.curk@fri.uni-lj.si>
2008-09-28 11:15:45 +0000

    Makefile.depends is generated automatically from setup.py

--- setup.py
+++ setup.py
@@ -10,17 +10,47 @@ for (dirp, dirns, n) in os.walk('doc'):
 		dirn = dirn + '/'
 	docFiles.extend( [dirn + n1r for n1r in nr if '.svn' not in dirp + '/' + n1r] )
 
-module=Extension("_GOLib", sources=["source/go.c"])
+modules=[Extension("_GOLib", sources=["source/go.c"])]
 
-setup(name = "Genomics",
-      version = "1.0",
-      description = "Genomics extensions for Orange",
-      author="University of Ljubljana, AI lab",
-      author_email="tomaz.curk@fri.uni-lj.si",
-      ext_modules=[module],
-      packages = [ 'widgets', 'doc' ],
-      package_data = {'widgets': ['icons/*.png'], 'doc': docFiles},
-      extra_path="Genomics",
-      py_modules = [ 'go', 'obiKEGG', 'obiGsea', 'obiGeneMatch', 'obiData', 'obiGenomicsUpdate', 'stats', 'pstat', 'obiExpression', 'obiGO', 'obiProb' ],
-      scripts=["post_install_script.py"]
-      )
+def writeMakeFileDepends():
+	f = open("Makefile.depends", "wt")
+
+	includePaths = []
+	for ext in modules:
+		if ext.include_dirs <> []:
+			for p in ext.include_dirs:
+				includePaths.append( "-I%s" % (p))
+	includePaths = " ".join(includePaths)
+
+	if includePaths <> "":
+		f.write("COMPILEOPTIONSMODULES = %s\n" % (includePaths))
+
+	f.write("modules:")
+	for ext in modules:
+		f.write(" %s.so" % (ext.name))
+	f.write("\n")
+
+	for ext in modules:
+		objs = []
+		for s in ext.sources:
+			if s[-2:] == '.c' or s[-4:] == '.cpp':
+				objfname = os.path.splitext(os.path.join("..", s))[0] + ".o"
+				objs.append( objfname)
+		objs = " ".join(objs)
+		f.write("%s.so: %s\n" % (ext.name, objs))
+		f.write("\t$(LINKER) $(LINKOPTIONS) %s -o %s.so" % (objs, os.path.join("..", ext.name)))
+	f.close()
+
+if __name__ == "__main__":
+	setup(name = "Genomics",
+	      version = "1.0",
+	      description = "Genomics extensions for Orange",
+	      author="University of Ljubljana, AI lab",
+	      author_email="tomaz.curk@fri.uni-lj.si",
+	      ext_modules=modules,
+	      packages = [ 'widgets', 'doc' ],
+	      package_data = {'widgets': ['icons/*.png'], 'doc': docFiles},
+	      extra_path="Genomics",
+	      py_modules = [ 'go', 'obiKEGG', 'obiGsea', 'obiGeneMatch', 'obiData', 'obiGenomicsUpdate', 'stats', 'pstat', 'obiExpression', 'obiGO', 'obiProb' ],
+	      scripts=["post_install_script.py"]
+	      )
