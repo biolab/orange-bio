d7b7bcd42051cd6fe397ce54c7a243053d741953
tomazc <tomaz.curk@fri.uni-lj.si>
2008-10-13 22:00:54 +0000

    FDR and Bonferroni multiple-testing corrections.

--- obiProb.py
+++ obiProb.py
@@ -13,7 +13,7 @@ class LogBin(object):
         LogBin._lookup.extend(lookup)
         LogBin._max = max
         
-    def _logbin(self, n ,k):
+    def _logbin(self, n, k):
         if n >= self._max:
             self._extend(n + 100)
         return self._lookup[n] - self._lookup[n - k] - self._lookup[k]
@@ -64,6 +64,29 @@ class Hypergeometric(LogBin):
         result = sum(math.exp(self._logbin(m, i) + self._logbin(N - m, n - i)) for i in (range(k) if subtract else range(k, n+1)))
         result /= math.exp(self._logbin(N, n))
         return 1.0 - result if subtract else result
-        
-        
-            
+
+
+## to speed-up FDR, calculate ahead sum([1/i for i in range(1, m+1)]), for m in [1,100000]. For higher values of m use an approximation, with error less or equal to 4.99999157277e-006. (sum([1/i for i in range(1, m+1)])  ~ log(m) + 0.5772..., 0.5572 is an Euler-Mascheroni constant) 
+c = [1.0]
+for m in range(2, 100000):
+    c.append( c[-1] + 1.0/m)
+
+def FDR(p_values, q=0.05, dependent=False, m=None):
+    if not(m):
+        m = len(p_values)
+    if m == 0:
+        return []
+
+    if dependent: # correct q for dependent tests
+        k = c[m-1] if m <= len(c) else math.log(m) + 0.57721566490153286060651209008240243104215933593992
+        q = q/k
+
+    return [p for (i, p) in enumerate(p_values) if p <= (i+1.0)*q/m]
+
+def Bonferroni(p_values, q=0.05, m=None):
+    if not(m):
+        m = len(p_values)
+    if m == 0:
+        return []
+    q = q/float(m)
+    return [p for p in p_values if p <= q]
