d088f5f5529ac4177f37375ac9cc1cd37503b677
tomazc <tomaz.curk@fri.uni-lj.si>
2008-10-14 08:23:49 +0000

    do not update database on removal

--- post_install_script.py
+++ post_install_script.py
@@ -7,21 +7,24 @@ import orngEnviron
 try:
     import orngRegistry
     orngRegistry.addWidgetCategory("Orange Genomics", \
-        os.path.join(distutils.sysconfig.get_python_lib(),"Genomics","widgets"), \
-        "remove" not in sys.argv[1] if len(sys.argv)>1 else 1)
+        os.path.join(distutils.sysconfig.get_python_lib(),"Genomics"), \
+        "remove" not in sys.argv[1] if len(sys.argv) > 1 else 1)
 except Exception, detail:
     print "Error: ", Exception, detail
     print "Add-on could not be registered with Orange Canvas. Please, make sure to install Orange and Orange Canvas before installing this add-on."
     print
 
-try:
-    from OWWidget import *
-    import OWDatabasesUpdate
-    app = QApplication(sys.argv)
-    w = OWDatabasesUpdate.OWDatabasesUpdate(wantCloseButton=True, showAll=True, searchString="essential")
-    w.show()
-    w.exec_()
-except Exception, detail:
-    print "Error: ", Exception, detail
-    print "Widget OWDatabasesUpdate failed to run. Databases needed by Orange Genomics add-on can be downloaded and updated only in this widget."
-    # need to add support for update from text terminal
+if len(sys.argv) == 0 or len(sys.argv) > 1 and "remove" not in sys.argv[1]:
+    try:
+        import orngEnviron
+        from OWWidget import *
+        sys.path.insert(0, 'widgets') # temporary fix, until orngEnviron and orngRegistry are fixed, and automatically add paths to add-on's modules and widgets into sys.path
+        import OWUpdateGenomicsDatabases
+        app = QApplication(sys.argv)
+        w = OWUpdateGenomicsDatabases.OWUpdateGenomicsDatabases(wantCloseButton=True, showAll=True, searchString="essential")
+        w.show()
+        w.exec_()
+    except Exception, detail:
+        print "Error: ", Exception, detail
+        print "Widget OWUpdateGenomicsDatabases failed to run. Databases needed by Orange Genomics add-on can be downloaded and updated only in this widget."
+        # need to add support for update from text terminal
