b398ca01453eb099f3dedc9012f00269df1a3cde
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2008-11-25 15:34:18 +0000

    -added console update menu interface

--- post_install_script.py
+++ post_install_script.py
@@ -5,7 +5,7 @@ import os
 
 if len(sys.argv) <= 1 or len(sys.argv) > 1 and "remove" not in sys.argv[1]:
     try:
-        import orngEnviron
+        import orngEnviron, bla
         from OWWidget import *
         import OWUpdateGenomicsDatabases
         app = QApplication(sys.argv)
@@ -13,6 +13,8 @@ if len(sys.argv) <= 1 or len(sys.argv) > 1 and "remove" not in sys.argv[1]:
         w.show()
         w.exec_()
     except Exception, detail:
-        print "Error: ", Exception, detail
-        print "Widget OWUpdateGenomicsDatabases failed to run. Databases needed by Orange Genomics add-on can be downloaded and updated only in this widget."
+##        print "Error: ", Exception, detail
+##        print "Widget OWUpdateGenomicsDatabases failed to run. Databases needed by Orange Genomics add-on can be downloaded and updated only in this widget."
         # need to add support for update from text terminal
+        import orngServerFiles
+        orngServerFiles.consoleupdate()
