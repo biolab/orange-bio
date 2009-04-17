48ce86c0a68b52caaa3ebca2e76df31b1793001d
tomazc <tomaz.curk@fri.uni-lj.si>
2009-04-17 06:57:27 +0000

    calling of update after install had to be removed due to problems with the updating of environment variables when running the installers directly from the Internet Explorer. When core orange and bioinformatics are downloaded in batch and the Internet Explorer's "Open" option is used, the PATH and other variables set by the Qt installer (which is run from core orange installation) are not being seen by the second process spawned by IE - the installer for bionformatics.

--- post_install_script.py
+++ post_install_script.py
@@ -4,20 +4,10 @@ import sys
 import os
 
 if len(sys.argv) <= 1 or len(sys.argv) > 1 and "remove" not in sys.argv[1]:
-    try:
-        import orngEnviron
-        from OWWidget import *
-        import OWUpdateGenomicsDatabases
-        app = QApplication(sys.argv)
-        w = OWUpdateGenomicsDatabases.OWUpdateGenomicsDatabases(wantCloseButton=True, showAll=True, searchString="essential")
-        w.show()
-        w.exec_()
-    except Exception, detail:
-        ## on windows this is automaticaly run from the distutils installer
-        if sys.platform == "win32":
-            os.system("start " + sys.prefix+"""\\python.exe -c "import orngServerFiles; orngServerFiles.consoleupdate()" """)
-        else:
-            import orngServerFiles
-            orngServerFiles.consoleupdate()
-            
-        
+    print "To download and/or update the databases needed by Orange Bioinformatics"
+    print "please use the \"Update Genomics Databases\" widget in Orange Canvas"
+    print "or use the orngServerFiles module, e.g.:"
+    print
+    print "    import orngServerFiles"
+    print "    orgnServerFiles.consoleupdate()"
+    print
