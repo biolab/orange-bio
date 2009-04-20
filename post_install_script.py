fc8cbabe372b351028c0a94787cecbc73b3c88e6
tomazc <tomaz.curk@fri.uni-lj.si>
2009-04-20 11:06:19 +0000

    partially corrected

--- post_install_script.py
+++ post_install_script.py
@@ -4,10 +4,24 @@ import sys
 import os
 
 if len(sys.argv) <= 1 or len(sys.argv) > 1 and "remove" not in sys.argv[1]:
-    print "To download and/or update the databases needed by Orange Bioinformatics"
-    print "please use the \"Update Genomics Databases\" widget in Orange Canvas"
-    print "or use the orngServerFiles module, e.g.:"
-    print
-    print "    import orngServerFiles"
-    print "    orgnServerFiles.consoleupdate()"
-    print
+    try:
+        ## on windows this is automaticaly run from the distutils installer
+        if sys.platform == "win32":
+            os.system("start " + sys.prefix+"""\\python.exe -c "import orngServerFiles; orngServerFiles.consoleupdate()" """)
+        else:
+            import orngServerFiles
+            orngServerFiles.consoleupdate()
+
+        print "Database donwload was successful."
+        print
+        print "For further updates of the databases needed by Orange"
+        print "Bioinformatics please use the \"Update Genomics Databases\""
+        print "widget in Orange Canvas or use the orngServerFiles module, e.g.:"
+        print
+        print "    import orngServerFiles"
+        print "    orgnServerFiles.consoleupdate()"
+        print        
+    except Exception, detail:
+        print "Database donwload did not succeed! Please, retry later."
+        print
+
