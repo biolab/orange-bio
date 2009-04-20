a646b9051bd2791536f75674d863e192af47b159
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2009-04-20 14:29:36 +0000

    - added update_by_tags call

--- post_install_script.py
+++ post_install_script.py
@@ -4,6 +4,17 @@ import sys
 import os
 
 if len(sys.argv) <= 1 or len(sys.argv) > 1 and "remove" not in sys.argv[1]:
+    try:
+        print "Downloading essential genomics databases"
+        if sys.platform == "win32":
+            os.system("start " + sys.prefix+"""\\python.exe -c "import orngServerFiles; orngServerFiles.update_by_tags(tags=['essential'])" """)
+        else:
+            import orngServerFiles
+            orngServerFiles.update_by_tags(tags=["essential"])
+    except Exception, ex:
+        print ex
+        pass
+
     print "To download and/or update the databases needed by Orange"
     print "Bioinformatics please use the \"Update Genomics Databases\""
     print "widget in Orange Canvas or use the orngServerFiles module:"
