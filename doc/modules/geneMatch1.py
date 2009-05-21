038e9fdfab2a8c4f9daecb25fe844a4a505169dd
markotoplak <devnull@localhost>
2009-05-21 16:02:22 +0000

    obiGene: started writing the manual.

--- doc/modules/geneMatch1.py
+++ doc/modules/geneMatch1.py
@@ -4,15 +4,15 @@ import obiKEGG
 keggorg = obiKEGG.KEGGOrganism("mmu")
 kegg_genes = keggorg.get_genes() 
 
-targets = [ "Fndc4", "Itgb8", "Cdc34", "Olfr1403" ] 
+query = [ "Fndc4", "Itgb8", "Cdc34", "Olfr1403" ] 
 
 gm = obiGene.GMKEGG("mmu") #use KEGG aliases for gene matching
-gm.set_targets(targets)
+gm.set_targets(kegg_genes) #set KEGG gene aliases as targets
 
 pnames = keggorg.list_pathways()
 
-for keggname in kegg_genes:
-    match = gm.umatch(keggname)
+for name in query:
+    match = gm.umatch(name) # matched kegg alias or None
     if match:
-        pwys = keggorg.get_pathways_by_genes([keggname])
-        print match, "is in", [ pnames[p] for p in pwys ] 
+    	pwys = keggorg.get_pathways_by_genes([match])
+        print name, "is in", [ pnames[p] for p in pwys ] 
