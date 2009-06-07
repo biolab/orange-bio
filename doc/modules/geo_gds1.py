5c74f91b1641f1fa6e8193ae496fda9bbcecb0d5
blaz <blaz.zupan@fri.uni-lj.si>
2009-06-07 14:33:11 +0000

    minor changes

--- doc/modules/geo_gds1.py
+++ doc/modules/geo_gds1.py
@@ -1,16 +1,23 @@
+"""
+Print out some information on specific GEO's data set.
+Does not download the data set.
+"""
+
 import obiGEO
+import textwrap
 
-gds_info = obiGEO.GDSInfo()
-
-gds = gds_info["GDS10"]
+gdsinfo = obiGEO.GDSInfo()
+gds = gdsinfo["GDS10"]
 
 print "ID:", gds["dataset_id"]
 print "Features:", gds["feature_count"]
 print "Genes:", gds["gene_count"]
 print "Organism:", gds["platform_organism"]
 print "PubMed ID:", gds["pubmed_id"]
-print "Sample types:", ", ".join(set([sampleinfo["type"] for sampleinfo in gds["subsets"]]))
-
+print "Sample types:"
+for sampletype in set([sinfo["type"] for sinfo in gds["subsets"]]):
+    ss = [sinfo["description"] for sinfo in gds["subsets"] if sinfo["type"]==sampletype]
+    print "  %s (%s)" % (sampletype, ", ".join(ss))
 print
-print "Description"
-print gds["description"]
+print "Description:"
+print "\n".join(textwrap.wrap(gds["description"], 70))
