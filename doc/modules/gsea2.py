518d7d38b0fd613eead59f6f3fb871e54ff1e309
markotoplak <devnull@localhost>
2009-07-20 10:54:25 +0000

    obiGsea: updated documentation

--- doc/modules/gsea2.py
+++ doc/modules/gsea2.py
@@ -2,19 +2,24 @@ import obiDicty
 import obiGeneSets
 import obiGsea
 import orange
+import obiGene
 
-dbc = obiDicty.DatabaseConnection("http://asterix.fri.uni-lj.si/microarray/api/index.php?")
+dbc = obiDicty.DatabaseConnection()
 data = dbc.getData(sample='pkaC-', time="8")[0] #get first chip
 
 print "First 10 examples"
 for ex in data[:10]:
     print ex
 
-genesets =  obiGeneSets.collections([":go:ddi", ":kegg:ddi"])
-res = obiGsea.runGSEA(data, organism="ddi", minPart=0.05, geneSets=genesets, permutation="gene")
+matcher=obiGene.matcher([[obiGene.GMKEGG("ddi"),obiGene.GMDicty()]])
+
+genesets =  obiGeneSets.collections([":kegg:ddi"])
+res = obiGsea.runGSEA(data, matcher=matcher, minPart=0.05, geneSets=genesets, 
+    permutation="gene")
 
 print "GSEA results"
 print "%-40s %6s %6s %6s %7s" % ("LABEL", "NES", "P-VAL", "SIZE", "MATCHED") 
 for name,resu in res.items()[:10]: 
-    print "%-40s %6.3f %6.3f %6d %7d" % (name[:30], resu[1], resu[2], resu[4], resu[5]) 
+    print "%-40s %6.3f %6.3f %6d %7d" % (name[:30], resu["nes"], resu["p"], 
+        resu["size"], resu["matched_size"]) 
 
