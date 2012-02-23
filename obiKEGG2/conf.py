8d5d5cc9fe0a3c6734d4ac7f6813def6d8441318
ales_erjavec <devnull@localhost>
2012-02-23 10:58:44 +0100

    Use Orange buffer dir for obiKEGG2 files.

--- obiKEGG2/conf.py
+++ obiKEGG2/conf.py
@@ -8,11 +8,13 @@ mostly just caching settings
 import os
 import ConfigParser
 from StringIO import StringIO
-
+from Orange.misc import serverfiles
+kegg_dir = serverfiles.localpath("KEGG2")
 
 default = """
 [cache]
-path = %(home)s/.obiKEGG/
+# path = %(home)s/.obiKEGG/
+path = %(kegg_dir)s/
 store = sqlite3
 kegg_invalidate = always
 
@@ -22,14 +24,16 @@ transport = urllib2
 
 """
 
-#_defaults_env = dict(default)
-#_defaults_env.update(dict(os.environ))
+# Orange kegg files dir
+env = dict(os.environ)
+env["kegg_dir"] = kegg_dir
 
-parser = ConfigParser.ConfigParser(dict(os.environ))#_defaults_env)
-# TODO: global settings
+parser = ConfigParser.ConfigParser(env)
+
 
 parser.readfp(StringIO(default), "default")
 
+# TODO: global settings rc file
 parser.read([os.path.expanduser("~/.obiKEGG/rc.cfg")])
 
 params = {}
