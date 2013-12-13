3721085c83b8e729ae2700b458d212a819cf60bb
markotoplak <devnull@localhost>
2013-12-13 15:57:51 +0100

    Remove reference that was converted into rst.

--- docs/reference-html/geo_gds3.py
+++
@@ -1,8 +0,0 @@
-import obiGEO
-
-gds = obiGEO.GDS("GDS1676")
-data = gds.getdata(sample_type="infection")
-print "Genes: %d, Samples: %d" % (len(data), len(data.domain.attributes))
-
-for a in data.domain.attributes:
-    print a.name, a.attributes
--- docs/reference-html/geo_gds4.py
+++
@@ -1,9 +0,0 @@
-import orngServerFiles
-import glob
-import re
-
-filenames = glob.glob(orngServerFiles.localpath("GEO") + "/GDS*.soft.gz")
-m = re.compile("(GDS[0-9]*).soft")
-print "%d data files cached:" % len(filenames)
-print " ".join([m.search(fn).group(1) for fn in filenames])
-
