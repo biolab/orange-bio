4c49013af97e349561ba65cfe64607c9a64bd94c
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2014-02-21 11:32:48 +0100

    Cleared the namespace package.

--- orangecontrib/bio/__init__.py
+++ orangecontrib/bio/__init__.py
@@ -1,36 +1,4 @@
 # namespace stub
 __import__("pkg_resources").declare_namespace(__name__)
 
-import warnings
-
-alreadyWarned = False
-disabledMsg = "Some features will be disabled due to failing modules\n"
-def _import(name):
-    global alreadyWarned
-    try:
-        __import__(name, globals(), locals(), [], -1)
-    except ImportError, err:
-        warnings.warn("%sImporting '%s' failed: %s" %
-            (disabledMsg if not alreadyWarned else "", name, err),
-            UserWarning, 2)
-        alreadyWarned = True
-
-_import("arrayexpress")
-_import("biomart")
-_import("gene")
-_import("geneset")
-_import("geo")
-_import("go")
-_import("gsea")
-_import("kegg")
-_import("mesh")
-_import("taxonomy")
-_import("omim")
-_import("ontology")
-_import("ppi")
-_import("utils")
-
-del _import
-del alreadyWarned
-del disabledMsg
-del warnings
+# NOTE: Keep this module empty
