biolab/orange-bio

commit a1fc2cfa83fda3037f66a67e62c82f5a923d86ad

OWSetEnrichment: Improve memory usage
Replace `set` objects in enrichemnt results with lists.
Clear state in onDeleteWidget method.

orangecontrib/bio/widgets3/OWSetEnrichment.py
@@ -947,6 +947,7 @@ class OWSetEnrichment(widget.OWWidget):
     def onDeleteWidget(self):
         if self.state is not None:
             self._cancelPending()
+            self.state = None
         self._executor.shutdown(wait=False)
 
 
@@ -958,8 +959,8 @@ from collections import namedtuple
 
 enrichment_res = namedtuple(
     "enrichment_result",
-    ["query_mapped",      #:: set
-     "reference_mapped",  #:: set
+    ["query_mapped",      #:: list
+     "reference_mapped",  #:: list
      "p_value",           #:: float
      "enrichment_score"   #:: float
      ]
@@ -983,7 +984,7 @@ def set_enrichment(target, reference, query,
     enrichment = query_p / ref_p if ref_p else np.nan
 
     return enrichment_res(
-        query_mapped, reference_mapped,
+        list(query_mapped), list(reference_mapped),
         prob.p_value(len(query_mapped), len(reference),
                      len(reference_mapped), len(query)),
         enrichment

commit 996c850f5e614951beda4da2bb0ddb7b52761033

Fix an TypeError when selecting and commiting multiple rows
This error was introduced in a1fc2cfa83fda3037f66a67e62c82f5a923d86ad

orangecontrib/bio/widgets3/OWSetEnrichment.py
@@ -924,7 +924,8 @@ class OWSetEnrichment(widget.OWWidget):
         rows = self.annotationsChartView.selectionModel().selectedRows(0)
         selected = [model.item(index.row(), 0) for index in rows]
         mapped = reduce(operator.ior,
-                        (item.enrichment.query_mapped for item in selected),
+                        (set(item.enrichment.query_mapped)
+                         for item in selected),
                         set())
         assert self.state.namematcher.done()
         matcher = self.state.namematcher.result()